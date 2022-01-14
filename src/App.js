5f2b190e9cd8c5b876e647811645f34a53ffa42a
rased100 <rased100@gmail.com>
2022-01-14 22:44:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import MyLineChart from './components/MyLineChart/MyLineChart';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>hello rechart</h2>
+      <MyLineChart></MyLineChart>
     </div>
   );
 }
